8ddd317818f4c7921a0599a51e508b8bea7227b3
Alexander Bortsov <A.bortsovv@yandex.ru>
2024-02-20 21:56:47 +0300

    Junit4 test create

---
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void RemainTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void RemainTestWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+}
